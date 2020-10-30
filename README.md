54a48c6575179c57bec74623e50a6aa4e3e8da0c
pjdip <pjdip1@gmail.com>
2020-10-30 19:48:29 -0400

    moving files around, addingEventListeners, relocating and getting more specific with pseudocode

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Philip-DiPaula-OSU-bootcamp-hwk4
 
 
+
+event delegation:
+https://davidwalsh.name/event-delegate
+
+list writing to the document:
 https://getbutterfly.com/generate-html-list-from-javascript-array/
--- assets/script.js
+++ assets/script.js
@@ -1,54 +1,85 @@
 /* 
-
 countdown timer
     starts at given time
     counts down
     deduct 10 seconds on wrong answer
     send user to end page when hit 0
 
-start button
-    hide start container
-    reveal 1st question container
-    initiate timer
-
-answer buttons
-    hide question
-    reveal next question/end
-    adjust timer if incorrect
-    p element: display correct or incorrect for 3 seconds
-
 display score
-
-submit button
-    store initials
-    append to high score list
-    switch to high score page
 
 5) high schore page
     list element: initials + score
-    return to home button
-        when clicked:
-            reset timer to 0
-            return to start page
-    clear scores button
-        when clicked:
-            clear high score list
 
 bonus: multiple question types: true/false and multiple choice */
 
-var returnBtn = document.getElementById("return");
-returnBtn.addEventListener("click", function() {
-    window.location.href='https://pjdip.github.io/Philip-DiPaula-OSU-bootcamp-hwk4/';
+var scoreList = [];
+
+
+document.getElementById("start").addEventListener("click", function() {
+    /* 
+    hide startSection
+    reveal question1
+    initiate timer
+    */
 })
 
-function updateHighScores(alias, score, scoreList) {
+document.getElementById("answers1").addEventListener("click", function(event) {
     /* 
-    score entry object: stores alias and score
-    list of score entry objects
-    create new score entry
+    hide question1
+    reveal quesiton2
+    adjust timer as needed
+    display correct/incorrect for 3 seconds
+    */
+});
+
+document.getElementById("answers2").addEventListener("click", function(event) {
+    /* 
+    hide question2
+    reveal quesiton3
+    adjust timer as needed
+    display correct/incorrect for 3 seconds
+    */
+});
+
+document.getElementById("answers3").addEventListener("click", function(event) {
+    /* 
+    hide question3
+    reveal quesiton4
+    adjust timer as needed
+    display correct/incorrect for 3 seconds
+    */
+});
+
+document.getElementById("answers4").addEventListener("click", function(event) {
+    /* 
+    hide question4
+    reveal endSection
+    adjust timer as needed
+    stop timer
+    display correct/incorrect for 3 seconds
+    */
+});
+
+document.getElementById("submit").addEventListener("click", updateHighScores);
+
+document.getElementById("return").addEventListener("click", function() {
+    /* reset the timer to 0 */
+    window.location.href='https://pjdip.github.io/Philip-DiPaula-OSU-bootcamp-hwk4/';
+});
+
+document.getElementById("clear").addEventListener("click", function(scoreList) {
+    /* delete high score list items 
+    append list to document
+    */
+});
+
+function updateHighScores(alias, score, scoreList) {
+
+    /* 
+    create score entry object: stores alias and score
+    append to score list
     sort score entry list by score value
     append list to document
-
     */
 
     var scoreEntry = document.createElement("li");
@@ -57,4 +88,6 @@ function updateHighScores(alias, score, scoreList) {
 
     scores.appendChild(scoreEntry);
     scoreEntry.setAttribute("style", " color:white; background: #666666; padding: 5px; margin-left: 35px;");
+
+    window.location.href='https://pjdip.github.io/Philip-DiPaula-OSU-bootcamp-hwk4/highScorePage.html';
 }
