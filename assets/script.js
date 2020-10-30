9dd293feda986c10cdc211cc9f68366ee62f398e
pjdip <pjdip1@gmail.com>
2020-10-30 16:08:57 -0400

    folders created, html skeleton constructed, initial pseudocode written

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+PseudoCode:
+1) all pages
+    a) View High Scores
+    b) Timer
+2) start page
+    a) quiz header
+    b) quiz description
+    c) start button
+3) questions pages
+    a) question
+    b) answer options
+    c) display correct or incorrect
+    d) timer adjustment
+    e) tally score
+    f) reveal next question/end page
+4) end page
+    a) quiz over message
+    b) display score
+    c) user input initials box
+    d) initials submission button
+    e) switch to high score page
+
+5) high schore page
+    a) High score list
+    b) return to home button
+    c) clear scores button
+
+bonus: display completion time with final score
