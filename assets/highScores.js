2fb0e5a9217f2ef9080a901e2eeaa0af674b257b
pjdip <pjdip1@gmail.com>
2020-11-02 19:45:17 -0500

    consolidating classes/ids and style to only what is needed

--- assets/highScores.js
+++ assets/highScores.js
@@ -1,25 +1,18 @@
 var highScoresEl = document.getElementById("highScores");
 var scoreList = [];
 
-function init() {
-    var retrievedScores = localStorage.getItem("scores");
-    if (retrievedScores !== null) {
-        scoreList = JSON.parse(retrievedScores);
-    }
-    renderScores();
+var retrievedScores = localStorage.getItem("scores");
+if (retrievedScores !== null) {
+    scoreList = JSON.parse(retrievedScores);
 }
 
-function renderScores() {
-    highScoresEl.innerHTML = "";
-    for (var i = 0; i < scoreList.length; i++) {
-        var li = document.createElement("li");
-        li.textContent = scoreList[i].initials + " - " + scoreList[i].points;
-        li.setAttribute("id", i);
-        highScoresEl.appendChild(li);
-    }
+highScoresEl.innerHTML = "";
+for (var i = 0; i < scoreList.length; i++) {
+    var li = document.createElement("li");
+    li.textContent = scoreList[i].initials + " - " + scoreList[i].points;
+    li.setAttribute("id", i);
+    highScoresEl.appendChild(li);
 }
-
-init();
 
 document.getElementById("return").addEventListener("click", function(event) {
     event.preventDefault();
@@ -30,9 +23,7 @@ document.getElementById("clear").addEventListener("click", function(event) {
     event.preventDefault();
     localStorage.removeItem("scores");
     var scoreLen = highScoresEl.childElementCount;
-    for (var child = 0; child < scoreLen; child++) {
-        highScoresEl.removeChild(document.getElementById(child.toString()));
-    } 
-/*     deal with the bubbling and undefined ending time */
-   
+    for (var j = 0; j < scoreLen; j++) {
+        highScoresEl.removeChild(document.getElementById(j.toString()));
+    }   
 });
--- assets/script.js
+++ assets/script.js
@@ -6,8 +6,8 @@ timeEl.textContent = 0;
 
 var secondsLeft = 75;
 var finalTime;
-var scoreList = [];
 
+var scoreList = [];
 var retrievedScores = localStorage.getItem("scores");
 if (retrievedScores !== null) {
     scoreList = JSON.parse(retrievedScores);
@@ -30,9 +30,39 @@ function reveal(hidden) {
     document.getElementById(hidden).classList.add("revealed");
 }
 
+document.getElementById("start").addEventListener("click", function(event) {
+    event.preventDefault();
+    hide("startSection");
+    reveal("question1");
+    timeEl.textContent = secondsLeft;
+    var timeInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft;
+
+        document.getElementById("answers4").addEventListener("click", function(event) {
+            event.preventDefault();
+            if (event.target.matches("button") && !event.target.matches("li")) {
+                clearInterval(timeInterval);
+            }
+        });
+
+        if (secondsLeft === 0) {
+            finalTime = 0;
+            timeEl.textContent = finalTime;
+            scoreEl.textContent = finalTime;
+            clearInterval(timeInterval);
+            hide("question1");
+            hide("question2");
+            hide("question3");
+            hide("question4");
+            reveal("endSection");
+        }
+    }, 1000);
+});
+
 function answerFeedback(accuracy) {
     reveal(accuracy);
-    var displayTime = 3;
+    var displayTime = 2;
     var timeInterval1 = setInterval(function() {
         displayTime--;
 
@@ -61,43 +91,6 @@ function answerCheck(evt, ans, prevSection, nextSection) {
     }
 }
 
-function storeScores() {
-    var stringifiedScores = JSON.stringify(scoreList);
-    localStorage.setItem("scores", stringifiedScores);
-}
-
-document.getElementById("start").addEventListener("click", function(event) {
-    event.preventDefault();
-    hide("startSection");
-    reveal("question1");
-    timeEl.textContent = secondsLeft;
-    var timeInterval = setInterval(function() {
-        secondsLeft--;
-        timeEl.textContent = secondsLeft;
-
-        document.getElementById("answers4").addEventListener("click", function(event) {
-            event.preventDefault();
-            if (event.target.matches("button") && !event.target.matches("li")) {
-                finalTime = secondsLeft;
-                timeEl.textContent = finalTime;
-                scoreEl.textContent = finalTime;
-                clearInterval(timeInterval);
-            }
-        })
-
-        if (secondsLeft === 0) {
-            finalTime = 0;
-            timeEl.textContent = finalTime;
-            clearInterval(timeInterval);
-            hide("question1");
-            hide("question2");
-            hide("question3");
-            hide("question4");
-            reveal("endSection");
-        }
-    }, 1000);
-});
-
 document.getElementById("answers1").addEventListener("click", function(event) {
     event.preventDefault();
     answerCheck(event, 2, "question1", "question2");
@@ -116,7 +109,18 @@ document.getElementById("answers3").addEventListener("click", function(event) {
 document.getElementById("answers4").addEventListener("click", function(event) {
     event.preventDefault();
     answerCheck(event, 0, "question4", "endSection");
+
+    if (event.target.matches("button") && !event.target.matches("li")) {
+        finalTime = secondsLeft;
+        timeEl.textContent = finalTime;
+        scoreEl.textContent = finalTime;
+    }
 });
+
+function storeScores() {
+    var stringifiedScores = JSON.stringify(scoreList);
+    localStorage.setItem("scores", stringifiedScores);
+}
 
 document.getElementById("submit").addEventListener("click", function(event) {
     event.preventDefault();
